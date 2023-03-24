3461628bee2156fbce6a06d0ef88543511c3eb4f
alissonmaia <alissonmaiadasilva28@gmail.com>
2023-03-23 23:00:08 -0300

    Meu Primeiro deploy

---
+++ script.bat
@@ -0,0 +1,18 @@
+echo "criando as imagens ....... "
+
+docker build -t alissonmaia/projeto-backend:1.0 bakcend/.
+docker build -t alissonmaia/projeto-database:1.0 database./
+
+echo "realizando o push das imagens   ...."
+
+
+docker push alissonmaia/projeto-backend:1.0
+docker push alissonmaia/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes  ....."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments ......"
+kubectl apply -f ./deployment.yml
+
